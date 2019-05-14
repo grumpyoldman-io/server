058ee754f757fe2f355971808970a398fb6bcd74
Marvin de Bruin <hello@grumpyoldman.io>
2019-05-14 11:49:39 +0300

    Updated packages and rebuilt app

--- src/communication.js
+++ src/communication.js
@@ -4,31 +4,38 @@ import '../config/env'
 import mqtt from 'mqtt'
 
 const topics = Object.keys(process.env)
-  .filter((envVar) => envVar.startsWith('MQTT_BUTTON_'))
-  .map((envVar) => ({
+  .filter(envVar => envVar.startsWith('MQTT_BUTTON_'))
+  .map(envVar => ({
     id: process.env[envVar],
     button: parseInt(envVar.replace('MQTT_BUTTON_', ''))
   }))
 
-const init = (emit) => new Promise((resolve) => {
-  const client = mqtt.connect(`mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`)
+const init = emit =>
+  new Promise(resolve => {
+    const client = mqtt.connect(
+      `mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`
+    )
 
-  client.on('message', (topic) => {
-    const match = topics.find(({ id }) => id === topic)
-    if (match) {
-      emit(match.button)
-    }
-  })
-
-  client.on('connect', () => {
-    topics.forEach(({ id }) => {
-      client.subscribe(id)
+    client.on('message', topic => {
+      const match = topics.find(({ id }) => id === topic)
+      if (match) {
+        emit(match.button)
+      }
     })
 
-    console.log(`listening to mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT} for buttons ${topics.map(({button}) => button).join(',')}`)
+    client.on('connect', () => {
+      topics.forEach(({ id }) => {
+        client.subscribe(id)
+      })
 
-    resolve()
+      console.log(
+        `listening to mqtt://${process.env.MQTT_HOST}:${
+          process.env.MQTT_PORT
+        } for buttons ${topics.map(({ button }) => button).join(',')}`
+      )
+
+      resolve()
+    })
   })
-})
 
 export default init
